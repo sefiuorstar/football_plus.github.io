9346b964196d3f786dfa4a0139ae0f2ec8eb9619
sefiuorstar <39660202+sefiuorstar@users.noreply.github.com>
2020-11-10 20:25:00 +0100

    Update test.php

--- test.php
+++ test.php
@@ -2,8 +2,8 @@
     {
        "name":"test",
        "type":"live",
-       "link":"https://api.tvmak.com/api_live.php",
-       "icon":"https://android.rediptv2.com/ch.php?usercode=2778340158&pid=1&mac=B89BED0206E0",
+       "link":"https://android.rediptv2.com/ch.php?usercode=2778340158&pid=1&mac=B89BED0206E0",
+       "icon":"https://images-na.ssl-images-amazon.com/images/I/31FkCiLyUYL._SY400_.png",
        "header":"TV LIVE"
     }
  ]
