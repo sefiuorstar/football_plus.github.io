50fc47544e164b913a74cb16449dd7212164bd97
sefiuorstar <39660202+sefiuorstar@users.noreply.github.com>
2020-11-10 20:16:23 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 [
     {
-       "title":"test",
+       "name":"test",
        "type":"live",
        "link":"https://api.tvmak.com/api_live.php",
        "icon":"https://android.rediptv2.com/ch.php?usercode=2778340158&pid=1&mac=B89BED0206E0",
