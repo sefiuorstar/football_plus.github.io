c9b2c7b721a73cabbe5996554a03cf2bfb7f18fb
sefiuorstar <39660202+sefiuorstar@users.noreply.github.com>
2020-11-10 20:35:19 +0100

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
     {
        "name":"test",
        "type":"live",
-       "link":"https://android.rediptv2.com/ch.php?usercode=2778340158&pid=1&mac=B89BED0206E0",
+       "link":"https://android.rediptv2.com/ch.php?usercode=4834324330&pid=1&mac=B89BED025039",
        "icon":"https://images-na.ssl-images-amazon.com/images/I/31FkCiLyUYL._SY400_.png",
        "header":"TV LIVE"
     }
